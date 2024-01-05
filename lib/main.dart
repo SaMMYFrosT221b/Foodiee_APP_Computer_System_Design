5d935eeeeb6886212432f30d781d520f5ebc8a53
SaMMYFrosT221b <ratnakarg@iitbhilai.ac.in>
2024-01-05 13:36:51 +0530

    Added some constants and splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:foodiee/utlis/constants/colors.dart';
 import 'package:foodiee/utlis/theme/theme.dart';
 
+import 'app.dart';
+
 void main() {
+  // Todo: Add Widget binding
+  // Todo: Init Local Storage
+  // Todo: Await native Splash
+  // Todo: Initialize Firebase
+  // Todo: Initialize Authentication
+
   runApp(const App());
-}
-
-class App extends StatelessWidget {
-  const App({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      themeMode: ThemeMode.system,
-      theme: TAppTheme.lightTheme,
-      darkTheme: TAppTheme.darkTheme,
-    );
-  }
 }
---
+++ lib/utlis/constants/image_strings.dart
@@ -0,0 +1,12 @@
+/// This class contains all the App Images in String formats.
+
+class TImages {
+  // App Logos
+  static const String darkAppLogo =
+      "assets/logos/t-store-splash-Logo-black.png";
+  static const lightAppLogo = "assets/logos/t-store-splash-logo-white.png";
+
+// -- Social Logos _ _
+  static const String google = "assets/Logos/google-icon.png";
+  static const String facebook = "assets/logos/facebook-icon.png";
+}
